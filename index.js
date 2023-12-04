b0ccd1b6b6ebf83addfc479fb9b9fb5da106bc3d
Darshil Mahraur <darshilmahraur67@gmail.com>
2023-12-04 18:45:26 +0530

    .

--- index.js
+++ index.js
@@ -2,25 +2,53 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
+async function main() {
+  // open the database file
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+  // create our 'messages' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
 
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '/public/index.html'));
-});
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '/public/index.html'));
+  });
 
-// Socket.io
-// whatever we got from the user to the server
-io.on('connection', (socket) => {
-    socket.on('user-message', (msg) => {
-      io.emit('chat message', msg);
+  io.on('connection', (socket) => {
+    socket.on('chat message', async (msg) => {
+      let result;
+      try {
+        // store the message in the database
+        result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+      } catch (e) {
+        // TODO handle the failure
+        return;
+      }
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
     });
   });
 
-server.listen(9000, () => {
-  console.log('server running at http://localhost:9000');
-});
+  server.listen(9000, () => {
+    console.log('server running at http://localhost:9000');
+  });
+}
+
+main();
